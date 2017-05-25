4a3d2974b9b9ac714ee000a2c39eb081cc9afcb4
FaustinoAllison <a17035@bcc.unifal-mg.edu.br>
2017-05-25 19:40:05 -0300

    adição da multiplicação

--- calc.c
+++ calc.c
@@ -9,12 +9,16 @@ int subtrai (int x, int y){
 	//Alteração feita na branch fSubtrai
 	return x-y;
 }
-int multiplica (){}
+int multiplica (int x, int y){
+	//Alteração feita na branch fSmult
+	return x*y;
+}
 int divide (){}
 
 int main(){
 	printf("Hello git!\n\n");
 	printf("Soma: %d\n", soma(2,2));
 	printf("Subtração: %d\n", subtrai(2,2));
+	printf("Multiplicação: %d\n", multiplica(2,2));
 
 }
